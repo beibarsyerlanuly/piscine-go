0b980fb603bc96348bebd873c25dd021b38c53c1
Beibarsyerlanuly <damskali97@mail.ru>
2019-10-17 08:31:53 +0600

    DDDDDD

--- printdigits/isnegative.go
+++ printdigits/isnegative.go
@@ -1,26 +1,26 @@
 package main
 
-import "fmt"
+import "Println"
 
 func main() {
 	var a bool = true
 	var b bool = false
 	if a && b {
-		fmt.Printf("Line 1 - Condition is true\n")
+		Println("Line 1 - Condition is true\n")
 	}
 	if a || b {
-		fmt.Printf("F\n")
+		Println("F\n")
 	}
 
 	/* lets change the value of  a and b */
 	a = false
 	b = true
 	if a && b {
-		fmt.Printf("F\n")
+		Println("F\n")
 	} else {
-		fmt.Printf("F\n")
+		Println("F\n")
 	}
 	if !(a && b) {
-		fmt.Printf("T\n")
+		Println("T\n")
 	}
 }
