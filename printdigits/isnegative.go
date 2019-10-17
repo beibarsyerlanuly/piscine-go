aab6b282d643b3a3c4f5d88cccb93f1313b9e099
Beibarsyerlanuly <damskali97@mail.ru>
2019-10-17 08:25:41 +0600

    allddk

--- printdigits/isnegative.go
+++ printdigits/isnegative.go
@@ -3,24 +3,24 @@ package main
 import "fmt"
 
 func main() {
-   var a bool = true
-   var b bool = false
-   if ( a && b ) {
-      fmt.Printf("Line 1 - Condition is true\n" )
-   }
-   if ( a || b ) {
-      fmt.Printf("F\n" )
-   }
-   
-   /* lets change the value of  a and b */
-   a = false
-   b = true
-   if ( a && b ) {
-      fmt.Printf("F\n" )
-   } else {
-      fmt.Printf("F\n" )
-   }
-   if ( !(a && b) ) {
-      fmt.Printf("T\n" )
-   }
+	var a bool = true
+	var b bool = false
+	if a && b {
+		fmt.Printf("Line 1 - Condition is true\n")
+	}
+	if a || b {
+		fmt.Printf("F\n")
+	}
+
+	/* lets change the value of  a and b */
+	a = false
+	b = true
+	if a && b {
+		fmt.Printf("F\n")
+	} else {
+		fmt.Printf("F\n")
+	}
+	if !(a && b) {
+		fmt.Printf("T\n")
+	}
 }
