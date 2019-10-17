7815303812bd385e5c158a20c51cb770aad9261e
Beibarsyerlanuly <damskali97@mail.ru>
2019-10-17 12:36:40 +0600

    plsddfffd5

--- isnegative.go
+++ isnegative.go
@@ -4,12 +4,12 @@ import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
 
-	if nb < 0 {
+		if nb < 0 {
 
-		z01.PrintRune('T')
+				z01.PrintRune('T')
 
-	} else {
-		z01.PrintRune('F')
+		} else if int <= 0 {
+				z01.PrintRune('F')
 	}
 
 	z01.PrintRune(10)
