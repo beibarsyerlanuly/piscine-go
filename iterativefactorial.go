3a8f515fce2131de69555634dbbab92659a84d02
Beibarsyerlanuly <damskali97@mail.ru>
2019-10-21 14:58:07 +0600

    iterativefactorial.go

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,10 +2,16 @@ package main
 
 import "fmt"
 
+func IterativeFactorial(nb int) int { 
+	result :=0
+	for i:=0; i< nb + 1 ; i++ {
+
+	result = result + i
+	}
+	return result
+} 
 func main() {
-
-	result1 := 0 + 1 + 2 + 3 + 3 + 4 + 5 + 6
-
-	fmt.Println(result1)
-
-}
+	
+	result := 0 +1 +2 +3 +3 +4 +5 +6
+	fmt.Println(result)
+} 
