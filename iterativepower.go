7dc6206ed9c0fcbc96ac76b58056b3f47d86884e
Beibarsyerlanuly <damskali97@mail.ru>
2019-10-22 14:20:58 +0600

    iterativepower.go

---
+++ iterativepower.go
@@ -0,0 +1,16 @@
+package piscine
+
+func IterativePower(nb int, power int) int {
+
+	if power < 0 {
+		return 0
+
+	}
+	if power > 0 {
+
+		return nb * IterativePower(nb, power*1)
+	}
+
+	return 1
+
+}
