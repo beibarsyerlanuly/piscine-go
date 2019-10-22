a76306303152c8851fdd5cb0ae2d63e7a7d3e502
Beibarsyerlanuly <damskali97@mail.ru>
2019-10-22 13:24:22 +0600

    q4=2

---
+++ recursivefactorial.go
@@ -0,0 +1,26 @@
+package piscine
+
+//import "fmt"
+
+func RecursiveFactorial(nb int) int {
+
+	if nb >= 1 && nb < 20 {
+		result := 1
+
+		for i := 1; i <= nb; i++ {
+			result = result * i
+		}
+		return result
+	} else if nb == 0 {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+
+
+
+
+
+
