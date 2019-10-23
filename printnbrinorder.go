5a570b849028d7658be51c8038813803caa4a138
Beibarsyerlanuly <damskali97@mail.ru>
2019-10-23 14:20:56 +0600

    printnbrinorder.go

---
+++ printnbrinorder.go
@@ -0,0 +1,51 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbrInOrder(n int) {
+
+	var k []int
+
+	if n == 0 {
+
+		k = append(k, n)
+
+	}
+
+	index := 0
+
+	for n > 0 {
+
+		k = append(k, n%10)
+
+		n = n / 10
+
+		index++
+
+	}
+
+	for i := 0; i < index; i++ {
+
+		for j := i + 1; j < index; j++ {
+
+			if k[i] > k[j] {
+
+				t := k[i]
+
+				k[i] = k[j]
+
+				k[j] = t
+
+			}
+
+		}
+
+	}
+
+	for i := range k {
+
+		z01.PrintRune(rune(48 + k[i]))
+
+	}
+
+}
