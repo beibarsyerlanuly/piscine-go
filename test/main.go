c5d3f30516b8c39959ae87fb59f064e17b55385c
Beibarsyerlanuly <damskali97@mail.ru>
2019-10-24 19:01:10 +0600

    fffxdsad

--- test/main.go
+++ test/main.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	z01.PrintRune(piscine.FirstRune("Hello!"))
-	z01.PrintRune(piscine.FirstRune("Salut!"))
-	z01.PrintRune(piscine.FirstRune("Ola!"))
+	z01.PrintRune(piscine.LastRune("Hello!"))
+	z01.PrintRune(piscine.LastRune("Salut!"))
+	z01.PrintRune(piscine.LastRune("Ola!"))
 	z01.PrintRune('\n')
 }
